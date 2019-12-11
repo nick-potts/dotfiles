2ab878bbf01feff2677c823e2702697fb9d33bbc
Nick <github@nickpotts.com.au>
2019-12-11 20:29:06 +1000

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,6 +4,7 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
